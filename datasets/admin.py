2a16746a3c6a5d45190a39c96caad4b0f2c5ace6
Gustavo Maronato <gustavomaronato@gmail.com>
2019-04-07 11:29:22 -0300

    registered export resources

--- datasets/admin.py
+++ datasets/admin.py
@@ -1,10 +1,53 @@
+from import_export import resources
+from import_export.admin import ExportMixin
 from django.contrib import admin
 from .models import Approved, Pending, Rejected, Deleted, NotEval, NotEvalAdmin
 # Register your models here.
 
 
-admin.site.register(Approved)
-admin.site.register(Pending)
-admin.site.register(Rejected)
-admin.site.register(Deleted)
-admin.site.register(NotEval, NotEvalAdmin)
+class ApprovedResource(resources.ModelResource):
+
+    class Meta:
+        model = Approved
+
+
+@admin.site.register(Approved)
+class ApprovedAdmin(ExportMixin, admin.ModelAdmin):
+    exclude = ('id',)
+    resource_class = ApprovedResource
+
+
+class PendingResource(resources.ModelResource):
+
+    class Meta:
+        model = Pending
+
+
+@admin.site.register(Pending)
+class PendingAdmin(ExportMixin, admin.ModelAdmin):
+    exclude = ('id',)
+    resource_class = PendingResource
+
+
+class RejectedResource(resources.ModelResource):
+
+    class Meta:
+        model = Rejected
+
+
+@admin.site.register(Rejected)
+class RejectedAdmin(ExportMixin, admin.ModelAdmin):
+    exclude = ('id',)
+    resource_class = RejectedResource
+
+
+class DeletedResource(resources.ModelResource):
+
+    class Meta:
+        model = Deleted
+
+
+@admin.site.register(Deleted)
+class DeletedAdmin(ExportMixin, admin.ModelAdmin):
+    exclude = ('id',)
+    resource_class = DeletedResource
